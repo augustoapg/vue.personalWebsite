ad13ab9473e812c8dc9e26246d075687847a38bb
Augusto Goncalez <augustoapg@gmail.com>
2019-09-30 21:37:55 -0400

    added resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,190 @@
 <template>
-    
+    <div class="container">
+        <div id="pdf-download">
+            <h1>Resume</h1>
+            <a href="RESUME_AUGUSTO_PERES.pdf" target="_blank">
+                Download Resume (PDF)</a>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+
+                <!-- Session for education -->
+                <h4 class="resume-session-title">Education</h4>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Systems Analyst Co-op 
+                            Program</span></li>
+                    <li><span  class="activity-place-date">Sheridan College 
+                            (01/2017 - Present)</span>
+                        <ul class="activity-desc">
+                            <li>3.94 GPA</li>
+                            <li>Expected graduation in 12/2019</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Mechanical Engineering
+                            - Bachelor Degree
+                        </span></li>
+                    <li><span  class="activity-place-date">UNESP - 
+                            Guaratingueta, SP/Brazil 
+                            (01/2010 - 12/2016)</span></li>
+                </ul>
+
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Scholarship Program
+                        </span></li>
+                    <li><span  class="activity-place-date">University of 
+                            Guelph (01/2014 - 12/2014)</span></li>
+                </ul>
+
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Lean Six Sigma - Black
+                            Belt Training</span></li>
+                    <li><span  class="activity-place-date">Dinamica 
+                            Treinamentos (120 hours)</span></li>
+                </ul>
+
+                <!-- Session for work experience -->
+
+                <h4 class="resume-session-title">Work Experience</h4>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Python Developer Research Assistant</span></li>
+                    <li><span  class="activity-place-date">Sheridan College
+                            (09/2019 - present)</span>
+                        <ul class="activity-desc">
+                            <li>Responsible for assisting in the design and development of a Dashboard for data visualization in real time</li>
+                            <li>Developing dashboard using Python with the GUI created with Tkinter.</li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Jr. Web Developer and QA Analyst</span></li>
+                    <li><span  class="activity-place-date">Government of Ontario
+                            (09/2018 - 12/2018) - (05/2019 - 08/2019)</span>
+                        <ul class="activity-desc">
+                            <li>Worked in an Agile based team for the Ontario Public Service intranet, which is used by around 60,000 public employees.</li>
+                            <li>Implemented new features and fixed bugs for most components of the website, which included front-end created in Vue, back-end using PHP with a customized Wordpress, and an API created using Go</li>
+                            <li>Worked with Docker and Vagrant for creating and debugging code in a local environment</li>
+                            <li>Developed automated test scripts using Selenium, Codeception and Bamboo</li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Co-op Developer</span></li>
+                    <li><span  class="activity-place-date">HitGrab Game Labs 
+                            (01/2018 - 04/2018)</span>
+                        <ul class="activity-desc">
+                            <li>Developed front and back end of a multiplayer word game prototype using JavaScript, jQuery, NodeJS and Socket.io.</li>
+                            <li>Organized scheduled tests and properly documented the results in order to improve the prototype based on the feedback provided.</li>
+                            <li>Responsible for organizing the project and delivering the results within the due date.</li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Peer Assisted Learning
+                            Leader</span></li>
+                    <li><span  class="activity-place-date">Sheridan College 
+                            (09/2017 - 12/2017)</span>
+                        <ul class="activity-desc">
+                            <li>Plan, prepare and lead one hour sessions 
+                                weekly on computer math where the most 
+                                important topics given in class are 
+                                revisited and practiced.</li>
+                            <li>Encourage class’s participation and guide 
+                                the students towards the solution.</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Manufacture Engineer 
+                            Intern</span></li>
+                    <li><span  class="activity-place-date">EATON Hydraulics 
+                            (02/2017 - 08/2017)</span>
+                        <ul class="activity-desc">
+                            <li>Worked on a team project to develop a new 
+                                data flux for the company's scrap, reducing 
+                                the time needed for analysis and increasing 
+                                the data reliability</li>
+                            <li>Interviewed the machine operators in order 
+                                to develop an Excel program with VBA to make
+                                it easier for the employees to input the
+                                production data, as well as increasing the 
+                                data reliability and facilitating the 
+                                analysis.</li>
+                            <li>Conducted and participated on 5S events to 
+                                organize and clean properly several work 
+                                stations.</li>
+                            <li>Presented to the company once a week advice 
+                                and news about health and safety.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h4 class="resume-session-title">Technical Skills</h4>
+                <ul class="experiences-skills-list">
+                    <li><span class="activity-title">Programming Languages and Frameworks</span>
+                        <ul class="activity-desc">
+                            <li>HTML, CSS, JavaScript</li>
+                            <li>Vue and Angular</li>
+                            <li>PHP</li>
+                            <li>JavaSE and J2EE</li>
+                            <li>Spring Boot</li>
+                            <li>Go</li>
+                            <li>Python</li>
+                            <li>SQL</li>
+                            <li>MongoDB</li>
+                            <li>NodeJS</li>
+                            <li>Android</li>
+                            <li>C# and ASP.NET</li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="experiences-skills-list">
+                    <li><span class="activity-title">System Analysis and 
+                            Project Management</span>
+                        <ul class="activity-desc">
+                            <li>Solid understanding of the Software 
+                                Development Life Cycle (SDLC)</li>
+                            <li>Strong knowledge of Unified Modeling Language (UML)</li>
+                            <li>Sound knowledge in project management 
+                                and lean six sigma techniques and tools, 
+                                such as DMAIC and A3 for Problem Solving</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <h4 class="resume-session-title">Other Skills</h4>
+                <ul class="activity-desc">
+                    <li>Experience working with Docker and Vagrant</li>
+                    <li>Sound knowledge and experience with Git</li>
+                    <li>Experience working with Selenium, Codeception and Bamboo</li>
+                    <li>Proficient in Microsoft Office applications (Word, PowerPoint, Access, Visio) with expertise in Excel</li>
+                </ul>
+
+                <h4 class="resume-session-title">Volunteer Experience</h4>
+                <ul class="experiences-skills-list">
+                    <li><span  class="activity-title">Volunteer 
+                            Teatcher</span></li>
+                    <li><span  class="activity-place-date">FORMARE - 
+                            Guaratingeta, SP, Brazil 
+                            (01/2016 - 07/2016)</span>
+                        <ul class="activity-desc">
+                            <li>Program that offer courses inside companies 
+                                to young people from socially vulnerable 
+                                families.</li>
+                            <li>Conducted Computing classes (mostly 
+                                Microsoft Excel)</li>
+                            <li>Assisted the students on their assignments 
+                                and doubts.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -13,5 +198,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    
+    #pdf-download {
+        margin-bottom: 20px;
+    }
+
+    #pdf-download h1{
+        display: inline;
+    }
+
+    #pdf-download a {
+        background-color: blue;
+        border: 1px solid black;
+        border-radius: 10px;
+        color: white;
+        float: right;
+        padding: 0.7em;
+        text-decoration: none;
+    }
+
+    .resume-session-title {
+        border-bottom: 3px double grey;
+    }
+
+    .experiences-skills-list {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .experiences-skills-list li {
+        list-style-type: none;
+    }
+
+    .activity-title {
+        font-weight: bold;   
+    }
+
+    .activity-location {
+        font-style: italic;
+    }
+
+    .activity-place-date {
+        font-style: italic;
+    }
+
+    .activity-desc li {
+        list-style-type: initial;
+    }
 </style>
