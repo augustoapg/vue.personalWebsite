45fe4e2bf7ad9fb8de4618efb5b28574ad1d34e4
Augusto Peres <augustoapg@gmail.com>
2022-04-04 15:32:39 -0400

    update AboutMe and Experiences with latest status

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,175 +1,187 @@
 <template>
-    <div>
-        <div class="hello-message">
-            <div class="hello-text">
-                <h1>Hello!</h1>
-                <h1>I am Augusto Peres</h1>
-                <h2>I am an Associate Developer at TD Bank</h2>
-                <h3 class="scroll-message">Scroll down to find out more about me</h3>
-            </div>
-            <canvas class="background"></canvas>
-        </div>
-        
-        <div class="container" id="aboutMe">
-            <h1><img src="../assets/icons8-user-64.png" alt="work icon">About Me</h1>
-            <div class="row about-me">
-                <div class="profile-pic col-md-3">
-                    <img src="./../assets/profilePicture.jpg" alt="my-picture"
-                            width="200">
-                </div>
-                <div class="col-md-9">
-                    <div class="about-me-text">
-                        <p class="lead">I am a Mechanical Engineer who decided to change careers because I
-                            fell in love with Computer Programming. I graduated from the Systems Analyst 
-                            program at Sheridan College in 2019 with high honors (GPA 3.94). I have over
-                            2 years of work experience with development, and I am always looking for a
-                            good challenge and for the chance to learn something new! 
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12" id="pdf-download">
-                <a href="./download/RESUME_AUGUSTO_PERES.pdf" target="_blank" class="download-resume">
-                    Download Resume (PDF)</a>
-            </div>
-        </div>
-    </div>
+	<div>
+		<div class="hello-message">
+			<div class="hello-text">
+				<h1>Hello!</h1>
+				<h1>I am Augusto Peres</h1>
+				<h2>I am an Associate Developer at TD Bank</h2>
+				<h3 class="scroll-message">
+					Scroll down to find out more about me
+				</h3>
+			</div>
+			<canvas class="background"></canvas>
+		</div>
+
+		<div class="container" id="aboutMe">
+			<h1>
+				<img src="../assets/icons8-user-64.png" alt="work icon" />About
+				Me
+			</h1>
+			<div class="row about-me">
+				<div class="profile-pic col-md-3">
+					<img
+						src="./../assets/profilePicture.jpg"
+						alt="my-picture"
+						width="200"
+					/>
+				</div>
+				<div class="col-md-9">
+					<div class="about-me-text">
+						<p class="lead">
+							I am a Mechanical Engineer who decided to change
+							careers because I fell in love with Computer
+							Programming. I graduated from the Systems Analyst
+							program at Sheridan College in 2019 with high honors
+							(GPA 3.94). I have over 3 years of work experience
+							with development, and I am always looking for a good
+							challenge and for the chance to learn something new!
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12" id="pdf-download">
+				<a
+					href="./download/RESUME_AUGUSTO_PERES.pdf"
+					target="_blank"
+					class="download-resume"
+				>
+					Download Resume (PDF)</a
+				>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Particles from 'particlesjs/dist/particles'
+import Particles from "particlesjs/dist/particles";
 export default {
-  
-
-  name: 'AboutMe',
-  props: {
-    
-  },
-  mounted: function() {
-    Particles.init({
-        selector: '.background',
-        connectParticles: true,
-        color: '#00ed00'
-      });
-      
-  }
-}
+	name: "AboutMe",
+	props: {},
+	mounted: function() {
+		Particles.init({
+			selector: ".background",
+			connectParticles: true,
+			color: "#00ed00",
+		});
+	},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .hello-message {
-        background-color: black;
-        width: 100%;
-        height: 100vh;
-        z-index: 0;
-        margin-top: -0.5em;
-    }
-    .hello-text {
-        font-family: 'Noto Sans';
-        margin: 0;
-        color: white;
-        font-size: 4vw;
-        text-align:center;
-        position: absolute;
-        top: 45%;
-        z-index: 1;
-        width: 100%;
-    }
-    .hello-text h1 {
-        margin: 0;
-        font-size: 2em;
-    }
-    .hello-text h2 {
-        font-size: 1.1em;
-        margin: 0.4em 0;
-    }
-    .hello-text .scroll-message {
-        font-size: 0.7em;
-    }
-    
-    .background {
-        height: 100%;
-        width:100%;
-        z-index: 0;
-    }
+.hello-message {
+	background-color: black;
+	width: 100%;
+	height: 100vh;
+	z-index: 0;
+	margin-top: -0.5em;
+}
+.hello-text {
+	font-family: "Noto Sans";
+	margin: 0;
+	color: white;
+	font-size: 4vw;
+	text-align: center;
+	position: absolute;
+	top: 45%;
+	z-index: 1;
+	width: 100%;
+}
+.hello-text h1 {
+	margin: 0;
+	font-size: 2em;
+}
+.hello-text h2 {
+	font-size: 1.1em;
+	margin: 0.4em 0;
+}
+.hello-text .scroll-message {
+	font-size: 0.7em;
+}
 
-    h1 {
-        text-align: center;
-    }
+.background {
+	height: 100%;
+	width: 100%;
+	z-index: 0;
+}
 
-    .about-me {
-        padding: 1.3em;
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-    }
+h1 {
+	text-align: center;
+}
 
-    .about-me-text {
-        display: table;
-        height: 13em;
-    }
+.about-me {
+	padding: 1.3em;
+	box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+		rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+}
 
-    .about-me-text p {
-        display: table-cell;
-        vertical-align: middle;
-    }
+.about-me-text {
+	display: table;
+	height: 13em;
+}
 
-    footer {
-        margin: 15px auto;
-        padding-right: 10px;
-        text-align: right;
-    }
+.about-me-text p {
+	display: table-cell;
+	vertical-align: middle;
+}
 
-    .profile-pic img {
-        border-radius: 20px;
-        display: block;
-        margin: 1em auto;
-        max-height: 200px;
-        max-width: 200px;
-        width: 100%;
-    }
+footer {
+	margin: 15px auto;
+	padding-right: 10px;
+	text-align: right;
+}
 
-    .download-resume {
-        color: #fff !important;
-        text-transform: uppercase;
-        text-decoration: none;
-        background: #00a300;
-        padding: 0.7em;
-        border-radius: 30px;
-        display: block;
-        border: none;
-        transition: all 0.4s ease 0s;
-        margin: 1.5em auto;
-        width: 17em;
-        text-align: center;
-    }
+.profile-pic img {
+	border-radius: 20px;
+	display: block;
+	margin: 1em auto;
+	max-height: 200px;
+	max-width: 200px;
+	width: 100%;
+}
 
-    .download-resume:hover {
-        background: #00ed00;
-        letter-spacing: 1px;
-        -webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
-        -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
-        box-shadow: 5px 40px -10px rgba(0,0,0,0.57);
-        transition: all 0.4s ease 0s;
-    }
+.download-resume {
+	color: #fff !important;
+	text-transform: uppercase;
+	text-decoration: none;
+	background: #00a300;
+	padding: 0.7em;
+	border-radius: 30px;
+	display: block;
+	border: none;
+	transition: all 0.4s ease 0s;
+	margin: 1.5em auto;
+	width: 17em;
+	text-align: center;
+}
 
-    @media only screen and (min-width: 768px) {
-        .hello-message {
-            background-color: black;
-            width: 100%;
-            height: 100vh;
-            z-index: 0;
-        }
+.download-resume:hover {
+	background: #00ed00;
+	letter-spacing: 1px;
+	-webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+	-moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+	box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+	transition: all 0.4s ease 0s;
+}
 
-        .hello-text {
-            font-family: 'Noto Sans';
-            margin: 0;
-            color: white;
-            font-size: 4vw;
-            text-align:center;
-            position: absolute;
-            top: 30%;
-            z-index: 1;
-            width: 100%;
-        }
-    }
+@media only screen and (min-width: 768px) {
+	.hello-message {
+		background-color: black;
+		width: 100%;
+		height: 100vh;
+		z-index: 0;
+	}
+
+	.hello-text {
+		font-family: "Noto Sans";
+		margin: 0;
+		color: white;
+		font-size: 4vw;
+		text-align: center;
+		position: absolute;
+		top: 30%;
+		z-index: 1;
+		width: 100%;
+	}
+}
 </style>
--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -1,125 +1,227 @@
 <template>
-    <div class="container" id="experiences">
-            <h1><img src="../assets/icons8-work-64.png" alt="work icon">Experiences</h1>
+	<div class="container" id="experiences">
+		<h1>
+			<img
+				src="../assets/icons8-work-64.png"
+				alt="work icon"
+			/>Experiences
+		</h1>
 
-        <div class="row">
-            <div class="col-12">
-                <h3>Developer Associate</h3>
-                <p class="location">TD</p>
-                <p class="date">(01/2020 - present)</p>
-                <ul class="activity-desc">
-                    <li><b>Central Innovation Agency (CIA)</b> - (01/2021 - present)</li>
-                    <ul class="rotation">
-                        <li>Supported and created new features for TD Gigs, an internal platform for short term assignments</li>
-                        <li>Developed scripts for spreadsheet automation</li>
-                        <li>Implemented POCs related to Distribution Lists listed in Active Directory (AD)</li>
-                        <li>Participated and helped organize One Day Builds, which were full days of coding to create a POC with business value</li>
-                        <li><i>Technologies Used:</i> Angular, NodeJS, Python, MySQL, Git</li>
-                    </ul>
-                    <li><b>Deposit Accounting Systems (DAS)</b> - (01/2020 - 12/2020)</li>
-                    <ul class="rotation">
-                        <li>Wrote scripts for data retrieval and automation</li>
-                        <li>Assisted with the suport of batch jobs</li>
-                        <li>Responsible for package handling, helping developers to transfer their changes from dev to production environments using Endevor and ServiceNow</li>
-                        <li><i>Technologies Used:</i> Python, VBA, SAS, JCL</li>
-                    </ul>
-                </ul>
-            </div>
-            <div class="col-12">
-                <h3>Python Developer Research Assistant</h3>
-                <p class="location">Sheridan College</p>
-                <p class="date">(09/2019 - 12/2019)</p>
-                <ul class="activity-desc">
-                    <li>Responsible for the design and development of a Dashboard for data visualization in real time</li>
-                    <li>Create a mobile app version of the Dashboard</li>
-                    <li><i>Technologies Used:</i> Python, Tkinter, MySQL, Android (Java)</li>
-                </ul>
-            </div>
-            <div class="col-12">
-                <h3>Jr. Web Developer and QA Analyst</h3>
-                <p class="location">Government of Ontario</p>
-                <p class="date">(05/2019 - 08/2019)</p>
-                <p class="date">(09/2018 - 12/2018)</p>
-                <ul class="activity-desc">
-                    <li>Worked in an Agile based team for the Ontario Public Service intranet</li>
-                    <li>Implemented new features and assisted for most components of the website</li>
-                    <li>Developed automated test scripts</li>
-                    <li><i>Technologies Used:</i> Vue, Go, PHP, Selenium, Bamboo, Docker, Vagrant</li>
-                </ul>
-            </div>
-            <div class="col-12">
-                <h3>Co-op Developer</h3>
-                <p class="location">HitGrab Game Labs </p>
-                <p class="date">(01/2018 - 04/2018)</p>
-                <ul class="activity-desc">
-                    <li>Develop a multiplayer word game prototype</li>
-                    <li><i>Technologies Used:</i> JavaScript, jQuery, NodeJS, MongoDB</li>
-                </ul>
-            </div>
-            <div class="col-12">
-                <h3>Peer Assisted Learning Leader</h3>
-                <p class="location">Sheridan College</p>
-                <p class="date">(09/2017 - 12/2017)</p>
-                <ul class="activity-desc">
-                    <li>Plan, prepare and lead one hour sessions 
-                                weekly on computer math where the most 
-                                important topics given in class are 
-                                revisited and practiced</li>
-                    <li>Encourage class’s participation and guide 
-                        the students towards the solution</li>
-                </ul>
-            </div>
-            <div class="col-12">
-                <h3>Manufacture Engineer Intern</h3>
-                <p class="location">EATON Hydraulics</p>
-                <p class="date">(02/2017 - 08/2017)</p>
-                <ul class="activity-desc">
-                    <li>Worked on a team project to develop a new 
-                                data flux for the company's scrap, reducing 
-                                the time needed for analysis and increasing 
-                                the data reliability</li>
-                    <li>Developed an Excel program with VBA to make
-                        it easier for the employees to input the
-                        production data, as well as increasing the 
-                        data reliability and facilitating the 
-                        analysis</li>
-                    <li>Conducted and participated on 5S events to 
-                        organize and clean properly several work 
-                        stations</li>
-                    <li>Presented to the company once a week advice 
-                        and news about health and safety</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+		<div class="row">
+			<div class="col-12">
+				<h3>Software Engineer (Associate Engineer II)</h3>
+				<p class="location">TD</p>
+				<p class="date">(01/2022 - present)</p>
+				<ul class="activity-desc">
+					<li>
+						Responsible for the creation of an application for
+						bankers that saves, loads, displays and exports data. In
+						charge of the Front-end
+					</li>
+					<li>
+						Participated in decisions on data structure,
+						architecture and code standards
+					</li>
+					<li>
+						Work closely with DevOps to assist with the deployment
+						in all environments
+					</li>
+					<li>
+						Use Figma and Jira to analyze design and business
+						requirements
+					</li>
+					<li>
+						<i>Technologies Used:</i> React, TypeScript, SCSS,
+						Python, Flask, MySQL, Git
+					</li>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Developer Associate</h3>
+				<p class="location">TD</p>
+				<p class="date">(01/2020 - 12/2021)</p>
+				<ul class="activity-desc">
+					<li>
+						<b>Corporate Investment Banking</b> - (07/2021 -
+						12/2021)
+					</li>
+					<ul class="rotation">
+						<li>
+							Assist with development of interactive internal apps
+							for bankers
+						</li>
+						<li>
+							Demoed applications to executives and Product Owners
+						</li>
+						<li>
+							Researched Glue42 API to implement requirements from
+							Product Owner
+						</li>
+						<li>
+							<i>Technologies Used:</i> React, Python, Flask,
+							Tailwind CSS, MySQL, Git, Glue42, AG-Grid
+						</li>
+					</ul>
+					<li>
+						<b>Central Innovation Agency (CIA)</b> - (01/2021 -
+						06/2021)
+					</li>
+					<ul class="rotation">
+						<li>
+							Supported and created new features for TD Gigs, an
+							internal platform for short term assignments
+						</li>
+						<li>Developed scripts for spreadsheet automation</li>
+						<li>
+							Implemented POCs related to Distribution Lists
+							listed in Active Directory (AD)
+						</li>
+						<li>
+							Participated and helped organize One Day Builds,
+							which were full days of coding to create a POC with
+							business value
+						</li>
+						<li>
+							<i>Technologies Used:</i> Angular, NodeJS, Python,
+							MySQL, Git
+						</li>
+					</ul>
+					<li>
+						<b>Deposit Accounting Systems (DAS)</b> - (01/2020 -
+						12/2020)
+					</li>
+					<ul class="rotation">
+						<li>Wrote scripts for data retrieval and automation</li>
+						<li>Assisted with the suport of batch jobs</li>
+						<li>
+							Responsible for package handling, helping developers
+							to transfer their changes from dev to production
+							environments using Endevor and ServiceNow
+						</li>
+						<li><i>Technologies Used:</i> Python, VBA, SAS, JCL</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Python Developer Research Assistant</h3>
+				<p class="location">Sheridan College</p>
+				<p class="date">(09/2019 - 12/2019)</p>
+				<ul class="activity-desc">
+					<li>
+						Responsible for the design and development of a
+						Dashboard for data visualization in real time
+					</li>
+					<li>Create a mobile app version of the Dashboard</li>
+					<li>
+						<i>Technologies Used:</i> Python, Tkinter, MySQL,
+						Android (Java)
+					</li>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Jr. Web Developer and QA Analyst</h3>
+				<p class="location">Government of Ontario</p>
+				<p class="date">(05/2019 - 08/2019)</p>
+				<p class="date">(09/2018 - 12/2018)</p>
+				<ul class="activity-desc">
+					<li>
+						Worked in an Agile based team for the Ontario Public
+						Service intranet
+					</li>
+					<li>
+						Implemented new features and assisted for most
+						components of the website
+					</li>
+					<li>Developed automated test scripts</li>
+					<li>
+						<i>Technologies Used:</i> Vue, Go, PHP, Selenium,
+						Bamboo, Docker, Vagrant
+					</li>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Co-op Developer</h3>
+				<p class="location">HitGrab Game Labs</p>
+				<p class="date">(01/2018 - 04/2018)</p>
+				<ul class="activity-desc">
+					<li>Develop a multiplayer word game prototype</li>
+					<li>
+						<i>Technologies Used:</i> JavaScript, jQuery, NodeJS,
+						MongoDB
+					</li>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Peer Assisted Learning Leader</h3>
+				<p class="location">Sheridan College</p>
+				<p class="date">(09/2017 - 12/2017)</p>
+				<ul class="activity-desc">
+					<li>
+						Plan, prepare and lead one hour sessions weekly on
+						computer math where the most important topics given in
+						class are revisited and practiced
+					</li>
+					<li>
+						Encourage class’s participation and guide the students
+						towards the solution
+					</li>
+				</ul>
+			</div>
+			<div class="col-12">
+				<h3>Manufacture Engineer Intern</h3>
+				<p class="location">EATON Hydraulics</p>
+				<p class="date">(01/2017 - 12/2017)</p>
+				<ul class="activity-desc">
+					<li>
+						Worked on a team project to develop a new data flux for
+						the company's scrap, reducing the time needed for
+						analysis and increasing the data reliability
+					</li>
+					<li>
+						Developed an Excel program with VBA to make it easier
+						for the employees to input the production data, as well
+						as increasing the data reliability and facilitating the
+						analysis
+					</li>
+					<li>
+						Conducted and participated on 5S events to organize and
+						clean properly several work stations
+					</li>
+					<li>
+						Presented to the company once a week advice and news
+						about health and safety
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Experiences',
-  props: {
-    
-  }
-}
+	name: "Experiences",
+	props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        text-align: center;
-        background-color: rgb(240, 240, 243);
-    }
+h3 {
+	text-align: center;
+	background-color: rgb(240, 240, 243);
+}
 
-    .location {
-        font-weight: bold;
-        margin: 0;
-    }
+.location {
+	font-weight: bold;
+	margin: 0;
+}
 
-    .date {
-        margin: 0;
-    }
+.date {
+	margin: 0;
+}
 
-    .rotation, .activity-desc {
-        margin: 1em 0;
-    }
+.rotation,
+.activity-desc {
+	margin: 1em 0;
+}
 </style>
